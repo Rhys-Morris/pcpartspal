7b24b9fa77ca00fc768918800950ee52b9b754f9
Rhys Morris <rhysmorris08@gmail.com>
2021-02-27 11:52:38 +1100

    Generate Devise User Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'static_pages#home'
 end
