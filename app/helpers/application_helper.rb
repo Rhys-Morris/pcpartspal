ff7eca32223e16206b6b671f95f3159fe7905242
Rhys Morris <rhysmorris08@gmail.com>
2021-03-01 14:48:27 +1100

    Created cards for each listing in index action and created show pages for each listing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def user_listing_count(user)
+        count = Listing.where("user_id": user.id).count
+        if count == 1
+            "#{count} listing for sale"
+        else
+            "#{count} listings for sale"
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :listings
   root 'static_pages#home'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :profiles, except: %i[ index ]
+  resources :listings
+  delete '/listings/(:id)', to: 'listings#destroy', as: 'delete_listing'
 end
