3caae5f5e0484437d75a45943f258bd93bc23261
Rhys Morris <rhysmorris08@gmail.com>
2021-03-04 09:54:30 +1100

    Stripe webhook method updated to pull out required information including receipt url

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,16 +5,25 @@ class PaymentsController < ApplicationController
   end
 
   def webhook
+    pp params["data"]
     payment_id = params[:data][:object][:payment_intent]
     payment = Stripe::PaymentIntent.retrieve(payment_id)
+    pp payment
+    payment_id = payment.id
+    receipt_url = payment.charges.receipt_url
     purchaser_id = payment.metadata.purchaser_id
     listing_id = payment.metadata.listing_id
+
+    # Switch listing to sold status
     purchased_listing = Listing.find(listing_id)
     purchased_listing["sold"] = true
-    
+
     if purchased_listing.save
-      flash[:success] = "Payment sold switched to true"
+      puts "----- Listing status sold switched to true ----- "
+    else
+      puts "----- Error in webhook -----"
     end
-    render plain: "Success"
+
+    # Add purchase entity
   end
 end
