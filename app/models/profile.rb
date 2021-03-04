e4450b7992811251f6db80f17120974e94380763
Rhys Morris <rhysmorris08@gmail.com>
2021-03-05 10:08:34 +1100

    Restored validations for Profile attributes, checked all validations passing

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,7 @@ class Profile < ApplicationRecord
   belongs_to :user, optional: true
   has_many :watches, dependent: :destroy
   has_many :listings, through: :watches
-  # validates :postcode, numericality: { greater_than: 200, less_than: 9945 }
-  # validates :bio, length: { maximum: 500 }
+  validates :location, presence: true
+  validates :postcode, numericality: { greater_than: 200, less_than: 9945 }
+  validates :bio, length: { maximum: 500 }
 end
