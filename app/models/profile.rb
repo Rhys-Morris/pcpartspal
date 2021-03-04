585bd34afd2c1fb6389c782f34db7fe4b32778a3
Rhys Morris <rhysmorris08@gmail.com>
2021-03-04 19:38:03 +1100

    Commented out postcode numericality validations as causing a bug with blank profile creationC

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,6 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_many :watches, dependent: :destroy
   has_many :listings, through: :watches
-  validates :postcode, numericality: { greater_than: 200, less_than: 9945 }
+  # validates :postcode, numericality: { greater_than: 200, less_than: 9945 }
   validates :bio, length: { maximum: 500 }
 end
