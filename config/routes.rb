15cf3f56dc427c2df0460523441e6a6ceba6cd8d
Rhys Morris <rhysmorris08@gmail.com>
2021-03-08 12:34:52 +1100

    Added options for postage for user to select

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/payments/success', to: 'payments#success', as: 'payment_success'
   post '/payments/webhook', to: 'payments#webhook'
+  get 'listings/filter', to: 'listings#filter', as: 'filter_listings'
+  get '/listings/(:id)/postage', to: 'listings#postage', as: 'postage'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :profiles, except: %i[ index ]
-  get 'listings/filter', to: 'listings#filter', as: 'filter_listings'
   resources :listings
   delete '/listings/(:id)', to: 'listings#destroy', as: 'delete_listing'
   post '/watches', to: 'watches#create', as: 'create_watch'
