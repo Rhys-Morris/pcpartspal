c925c636b2a98b24ddd39d207f53c0374aa45b7e
Rhys Morris <rhysmorris08@gmail.com>
2021-03-08 18:13:03 +1100

    Added template buttons for leaving feedback from purchases tab of profile

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -13,6 +13,10 @@ class Listing < ApplicationRecord
   validates :description, length: { maximum: 500 }
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :condition, presence: true
+  validates :length, presence: true, numericality: { greater_than: 0, less_than: 300 }
+  validates :height, presence: true, numericality: { greater_than: 0, less_than: 300 }
+  validates :width, presence: true, numericality: { greater_than: 0, less_than: 300 }
+  validates :weight, presence: true, numericality: { greater_than: 0, less_than: 150 }
 
   def self.filter(filtered_params)
     Listing.where(filtered_params)
